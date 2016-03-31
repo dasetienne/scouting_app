56b52a406c11f231781904c0b15bd839a7db7f57
dasetienne <etienne.leue@gmail.com>
2016-03-30 21:19:39 -0400

    Missing picture works, joint table works, cant submit team

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,14 +1,17 @@
 class Player < ActiveRecord::Base
+
   has_attached_file :photo, :styles =>
-    { :medium => "300x300>", :thumb => "100x100>", :small =>"30x30" },
-    :default_url => "public/images/missing.png"
+  { :medium => "300x300>", :thumb => "100x100>", :small =>"100x100>" },
+  :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :photo,
-    :content_type => /\Aimage\/.*\Z/
+  :content_type => /\Aimage\/.*\Z/
+
   belongs_to :author
   belongs_to :league
   belongs_to :team
   has_many :player_skills
   has_many :skills, through: :player_skills
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :team, presence: true
