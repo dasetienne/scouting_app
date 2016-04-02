30934518ba933937b2bd5364bcf5c13125aa85f8
dasetienne <etienne.leue@gmail.com>
2016-04-02 12:55:58 -0400

    Delete function and button added to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,4 +5,14 @@ class CommentsController < ApplicationController
 
     redirect_to player_path(@player)
   end
+
+  def destroy
+    @player = Player.find(params[:player_id])
+    @comment = @player.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to player_path(@player)
+
+
+  end
 end
